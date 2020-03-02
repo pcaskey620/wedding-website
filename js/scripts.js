100f4597bef053f39d86d2d6daf3413b601cf805
Parker <parker.caskey@gmail.com>
2020-03-02 12:25:08 -0700

    Updated script.js
    added correct lat and long

--- js/scripts.js
+++ js/scripts.js
@@ -259,7 +259,7 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var location = {lat: 22.5932759, lng: 88.27027720000001};
+    var location = {lat: 40.21268, lang: 106.48542};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: location,
