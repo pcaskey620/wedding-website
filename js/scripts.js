11a9eb6372c7fa3eb64cca2b911c329f5dc8bba7
Parker-Kalovaras <68713387+Parker-Kalovaras@users.noreply.github.com>
2021-03-10 14:22:05 -0700

    more pictures and embedded vimeo

--- js/scripts.js
+++ js/scripts.js
@@ -129,6 +129,13 @@ $(document).ready(function () {
         counter: true
     });
 
+    $('#video-gallery').lightGallery({
+        loadYoutubeThumbnail: true,
+        youtubeThumbSize: 'default',
+        loadVimeoThumbnail: true,
+        vimeoThumbSize: 'thumbnail_medium'
+    });
+
     /***************** Tooltips ******************/
     $('[data-toggle="tooltip"]').tooltip();
 
