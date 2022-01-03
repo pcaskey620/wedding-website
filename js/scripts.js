093fb9eaa5cab06f3b6d8a4b581412830b495c4b
Parker-Kalovaras <68713387+Parker-Kalovaras@users.noreply.github.com>
2022-01-03 13:32:47 -0700

    Updated scripts

--- js/scripts.js
+++ js/scripts.js
@@ -956,8 +956,8 @@ $(document).ready(function () {
         
         
         $('#alert-wrapper2').html(alert_markup('success', '<strong>Just a sec!</strong> We are saving your details.'));
-    
-        var postUrl = 'https://script.google.com/macros/s/AKfycbwJ-VbIfB4p2ES3HBDtbNYdIRNbnViNNrI4ED5i2MaKgqLL1_P6iEc5Coh1ZvIl1OAvYA/exec';
+ 
+        var postUrl = 'https://script.google.com/macros/s/AKfycbzQyr9ZP5lMBvnMkKcGFpHqmNmBQYeYgsKUrueFIlhCYYCXAgLkK1HhTeoqG4EtmIrHew/exec';
         $.post(postUrl, data)
             .done(function (data) {
                 console.log(data);
